6f7b8093785748763d4a74ec512ef6934b2d9c56
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2025-12-07 14:53:08 -0500

    Removed DB credentials

--- Backend/config.py
+++ Backend/config.py
@@ -3,6 +3,6 @@
 # MySQL Database Configuration
 # Replace with your actual MySQL database credentials
 MYSQL_HOST = 'localhost'  # Or your MySQL server IP/hostname
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'pledgxsupersecret123'
+MYSQL_USER = 'test'
+MYSQL_PASSWORD = 'Abc12345!'
 MYSQL_DB = 'nacc'  # The database where nacc_exam_questions table is located
